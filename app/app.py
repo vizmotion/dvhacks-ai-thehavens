6098d0ac686421041fd32d98ba9c1bb5071a1703
robertcoop <coopster@gmail.com>
2018-12-01 19:17:32 -0800

    Initial ERD D3 visualization

--- app/app.py
+++ app/app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/view_erd')
+def view_erd():
+    return render_template('view_erd.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
